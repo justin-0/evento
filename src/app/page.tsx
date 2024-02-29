3bbc8caf1f2973d148dc3ebceaf8de7568ec0439
Justin OConnell <itsjustinoconnell@gmail.com>
2024-02-29 10:56:25 +0000

    root page and footer styled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,28 +2,38 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex h-4/6 flex-col items-center justify-center px-5 md:px-24">
-      <div className="flex flex-col items-center rounded bg-green-500 p-8">
-        <h1 className="text-2xl font-bold lg:text-4xl">
+    <main className="flex h-5/6 flex-col items-center justify-center px-5 md:px-24">
+      <div className="flex h-3/6 flex-col items-center justify-center rounded bg-green-500 p-12">
+        <h1 className="text-xl font-bold lg:text-4xl">
           Find entertainment around you
         </h1>
         <p className="mt-2 font-light lg:text-xl">
-          Browse more than <span className="text-yellow-300/90">10,000</span>{" "}
-          events nationwide
+          Browse more than{" "}
+          <span className="italic text-yellow-300/80 underline">
+            10,000 events
+          </span>{" "}
+          nationwide
         </p>
-        <form action="" className="mt-5 w-5/6">
+        <form action="" className="mt-5 w-5/6 lg:w-3/6">
           <input
-            className="w-full rounded p-2 text-neutral-900 outline-none focus:outline-2 focus:outline-blue-600"
+            className="w-full rounded p-2 text-neutral-900 outline-none ring-yellow-300 focus:bg-white/95 focus:ring-2"
             type="text"
             placeholder="search events in city..."
             spellCheck={false}
           />
         </form>
-        <div className="mt-2 flex gap-x-4">
-          <span className="font-medium">Popular:</span>
-          <div className="flex gap-x-4">
-            <Link href="/events/london">London</Link>
-            <Link href="/events/swindon">Swindon</Link>
+        <div className="mt-2 flex gap-x-4 text-sm text-white/75">
+          <span className="">Popular:</span>
+          <div className="flex gap-x-2 font-medium">
+            <Link href="/events/london" className="transition hover:text-white">
+              London
+            </Link>
+            <Link
+              href="/events/swindon"
+              className="transition hover:text-white"
+            >
+              Swindon
+            </Link>
           </div>
         </div>
       </div>
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,11 @@
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="h-[calc(100vh-689px)] border-t border-neutral-500/30 px-5 md:px-24">
+      <div className="flex h-full items-center justify-start">
+        <small className="font-sm font-regular">
+          2420 Tinology &copy; all rights reserved
+        </small>
+      </div>
+    </footer>
+  );
 }
