9222de6e7cb566386ba16284fe311d163dde2182
Justin OConnell <itsjustinoconnell@gmail.com>
2024-03-02 11:54:06 +0000

    custom skeleton loader for event page

--- src/app/event/[slug]/loading.tsx
+++ src/app/event/[slug]/loading.tsx
@@ -1,5 +1,10 @@
+import Skeleton from "@/components/skeleton";
 import React from "react";
 
 export default function Loading() {
-  return <div>Loading</div>;
+  return (
+    <div className="flex flex-col items-center justify-center gap-10 p-10">
+      <Skeleton page={"event"} />
+    </div>
+  );
 }
--- src/components/skeleton.tsx
+++ src/components/skeleton.tsx
@@ -1,5 +1,20 @@
 import React from "react";
 
-export default function Skeleton() {
-  return <div />;
+type SkeletonProps = {
+  page: "event" | "events";
+};
+
+export default function Skeleton({ page }: SkeletonProps) {
+  return (
+    page === "event" && (
+      <>
+        <div className="h-[45vh] w-[75%] animate-pulse rounded bg-white/20"></div>
+        <div className="flex w-full flex-col items-center justify-center gap-5">
+          <div className="h-[5vh] w-[45%] animate-pulse rounded bg-white/20"></div>
+          <div className="h-[5vh] w-[45%] animate-pulse rounded bg-white/20"></div>
+          <div className="h-[5vh] w-[45%] animate-pulse rounded bg-white/20"></div>
+        </div>
+      </>
+    )
+  );
 }
